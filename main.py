65660ee0a10919fd6988c74b085e0b2decf079d3
Herman Lundby-Holen <hermanluho@gmail.com>
2026-01-28 15:18:17 +0100

    feat: crop to increase accuracy of digit detection

--- main.py
+++ main.py
@@ -11,6 +11,22 @@ if not camera.isOpened():
   print("unable to open camera. will exit. bye.")
   exit()
 
+# take one picture to select zoom area
+success, first_frame = camera.read()
+if success:
+  # opens window to drag mouse to select clock
+  rect = cv2.selectROI("choose zoom area", first_frame, fromCenter=False, showCrosshair=True)
+  cv2.destroyWindow("choose zoom area")
+
+  # rect returns (x, y, w, h). if w or h is 0, user didn't select anything
+  if rect[2] == 0 or rect[3] == 0:
+    zoom_active = False
+  else:
+    zoom_active = True
+    x,y,w,h = int(rect[0]), int(rect[1]), int(rect[2]), int(rect[3])
+else:
+  exit()
+
 while True:
   # read camera feed
   success, frame = camera.read()
@@ -19,6 +35,11 @@ while True:
   if not success:
     print("can't receive frame. exiting...")
     break
+
+  # apply zoom
+  if zoom_active:
+    # we slice the array to create a new, smaller image containing the clock
+    frame = frame[y:y+h, x:x+w]
 
   # feed camera to yolo model and get the results as a list
   results = model(frame, verbose=False)
