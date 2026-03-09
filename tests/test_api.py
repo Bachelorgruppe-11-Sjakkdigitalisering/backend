aa8001109f3da92c70df497349cab683eef3723e
Herman Lundby-Holen <hermanluho@gmail.com>
2026-03-09 16:26:37 +0100

    test: write test for setting and getting live games

--- tests/test_api.py
+++ tests/test_api.py
@@ -20,3 +20,35 @@ def get_session_override():
 app.dependency_overrides[get_session] = get_session_override
 
 client = TestClient(app)
+
+# ---- TEST SETUP AND TEARDOWN ----
+
+@pytest.fixture(autouse=True)
+def setup_and_teardown():
+  # run before test:
+  SQLModel.metadata.create_all(engine)
+  active_games.clear()
+
+  yield
+
+  # runs after the tests:
+  SQLModel.metadata.drop_all(engine)
+
+# ---- TESTS ----
+def test_set_and_get_live_game():
+  """Test the in-memory active_games logic."""
+  payload = {
+    "board_id": 1,
+    "fen": "startpos",
+    "white_time": "10:00",
+    "black_time": "10:00",
+    "is_active": True
+  }
+
+  post_response = client.post("/api/update", json=payload)
+  assert post_response.status_code == 200
+  assert post_response.json() == { "status": "updated", "board_id": 1 }
+
+  get_response = client.get("/api/game/1")
+  assert get_response.status_code == 200
+  assert get_response.json()["fen"] == "startpos"
