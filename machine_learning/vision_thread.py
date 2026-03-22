52ba340ba5846cbe49603a7ecf6438758a957392
Herman Lundby-Holen <hermanluho@gmail.com>
2026-03-22 15:20:02 +0100

    feat(vision): update vision thread to support finding and locking in board position

--- machine_learning/vision_thread.py
+++ machine_learning/vision_thread.py
@@ -2,6 +2,9 @@ import threading
 import queue
 import cv2
 import time
+import chessboard.chessboard as chessboard
+from ultralytics import YOLO
+from collections import deque
 
 class VisionThread(threading.Thread):
   """
@@ -15,10 +18,26 @@ class VisionThread(threading.Thread):
     self.is_running = threading.Event()
     self.cap = None
 
+    self.board_model = YOLO('brett.pt')
+
+    self.history = deque(maxlen=10)
+    self.M = None
+    self.M_inv = None
+    self.show_boxes = True
+
   def start_camera(self):
     self.cap = cv2.VideoCapture(self.camera_source)
     self.is_running.set()
     self.start() # Starts the run() method in the background
+
+  def lock_board(self):
+    """Locks the perspective on the board based on recent frames."""
+    self.M, self.M_inv = chessboard.lock_perspective(self.history)
+    if self.M is not None:
+      print("Vision: Board perspective locked successfully!")
+      self.show_boxes = False
+    else:
+      print("Vision: Not enough data to lock board yet.")
 
   def run(self):
     """The main loop running in the background thread."""
@@ -31,9 +50,22 @@ class VisionThread(threading.Thread):
       if not success:
         continue
 
-      # TODO: legg til YOLO her
-
       processed_frame = frame.copy()
+
+      if self.M_inv is None or self.show_boxes:
+        # Board is not locked
+        results = self.board_model(frame, conf=0.05, verbose=False, iou=0.1)
+
+        if self.show_boxes:
+          processed_frame = results[0].plot()
+
+        corners = chessboard.extract_corners(results)
+        if corners is not None:
+          self.history.append(corners)
+
+      if self.M_inv is not None:
+        # Board is locked
+        chessboard.draw_grid(processed_frame, self.M_inv)
 
       # Pass data to GUI queue
       # Clear out old frames if the GUI is reading too slowly
