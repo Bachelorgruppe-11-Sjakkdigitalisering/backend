fix: special move detection and board rotation

chessboard/chessboard.py
@@ -7,11 +7,11 @@ def sort_points(pts):
     pts = np.array(pts, dtype="float32")
     rect = np.zeros((4, 2), dtype="float32")
     s = pts.sum(axis=1)
-    rect[0] = pts[np.argmin(s)]
-    rect[2] = pts[np.argmax(s)]
+    rect[1] = pts[np.argmin(s)] # h1
+    rect[3] = pts[np.argmax(s)] # a8
     diff = np.diff(pts, axis=1)
-    rect[1] = pts[np.argmin(diff)]
-    rect[3] = pts[np.argmax(diff)]
+    rect[2] = pts[np.argmin(diff)] # h8
+    rect[0] = pts[np.argmax(diff)] # a1
     return rect
 
 def extract_corners(results):