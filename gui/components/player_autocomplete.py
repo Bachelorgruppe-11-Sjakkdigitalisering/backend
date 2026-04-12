04e478267f8ce092e5954924bbd84204e8af1c67
Herman Lundby-Holen <hermanluho@gmail.com>
2026-04-12 13:46:47 +0200

    feat: add debounce effect on player search

--- gui/components/player_autocomplete.py
+++ gui/components/player_autocomplete.py
@@ -18,4 +18,18 @@ class PlayerAutocomplete(ctk.CTkFrame):
 
   def _schedule_search(self, event):
     query = self.entry_search.get().strip()
-    print(f"{query}")
+
+    # Ignore search before user types more than two characters
+    if len(query) < 2:
+      self.dropdown_frame.pack_forget()
+      return
+    
+    if self.search_timer:
+      self.search_timer.cancel()
+
+    # Debounce for 0.3s so we don't search on every input
+    self.search_timer = threading.Timer(0.3, self._perform_search, args=[query])
+    self.search_timer.start()
+
+  def _perform_search(self, query):
+    print(query)
